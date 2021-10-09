7e484a52fab8524b42af7f42ffda7c8c56685c7d
Sanket Maheshwari <sanket@Sankets-MacBook-Pro.local>
2021-10-08 23:32:57 -0400

    second commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sanket",
+    description="A small package for tensorflow ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sanket-DataEnt/DVC_DL_Tensorflow_Repo",
+    author_email="sanketmaheshwari96@gmail.com",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires=[
+            "dvc",
+            "tensorflow",
+            "matplotlib",
+            "numpy",
+            "pandas",
+            "tqdm",
+            "PyYAML",
+            "boto3",
+    ]
+)
